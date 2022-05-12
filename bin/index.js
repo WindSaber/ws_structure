6d4feaef09737d85055e9446d4e5a43c7610d2ad
wind.saber@hotmail.com <NamaAmornp170891>
2022-05-12 17:34:05 -0500

    Intentos de Inquirer

--- bin/index.js
+++ bin/index.js
@@ -2,25 +2,47 @@
 
 const chalk = require("chalk");
 const boxen = require("boxen");
+const fse = require('fs-extra');
+var inquirer = require('inquirer');
+const {reiniciaMenu} = require('./displays')
+
+reiniciaMenu();
+
+inquirer
+    .prompt([
+        'adsadasdsadas sad sad sada sd'
+    ])
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
 
 const greeting = chalk.white.bold("Archivo Creado!");
 
 const boxenOptions = {
-    padding: 1,
-    margin: 1,
+    padding: 0,
+    margin: 0,
     borderStyle: "round",
     borderColor: "green",
-    backgroundColor: "#555555"
+    //backgroundColor: "#EFEFEF"
 };
 const msgBox = boxen(greeting, boxenOptions);
 
+// console.log(chalk.blue('queonda raza'));
 
-const fse = require('fs-extra');
 
-fse.outputFile('tmp/test.txt', 'Hey there!')
-    .then(() => {
-        console.log(msgBox);
-    })
-    .catch(err => {
-        console.error(err)
-    });
+// const fse = require('fs-extra');
+//
+// fse.outputFile('tmp/test.txt', 'Hey there!')
+//     .then(() => {
+// console.log(msgBox);
+//     })
+//     .catch(err => {
+//         console.error(err)
+//     });
